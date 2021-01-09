114676b5846b6a1bb139f8f3fbe3c25dfa895914
Kat Gomozova <kathy.gomozova@gmail.com>
2021-01-08 21:38:18 -0800

    Building front-end

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,10 +15,12 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
+    const is_devoured = false;
+    console.log(req.body.burger_name)
     burger.insertOne([
     "burger_name", "devoured"
   ], [
-    req.body.burger_name, req.body.devoured
+    req.body.burger_name, is_devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
